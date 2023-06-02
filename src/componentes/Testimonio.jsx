8c0cf76e42b126b2d86e9bbfdf5faa9e270c125e
Sammy Maldonado <sammyalejandro.m.p@gmail.com>
2023-06-02 16:56:54 -0400

    Proyecto Testimonios | Sammy Maldonado

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css'
+
+const Testimonio = (props) => {
+  return (
+    <div className='contenedor-testimonio'>
+      <img className='imagen-testimonio' 
+      src={`../src/imagenes/testimonio-${props.imagen}.png`} 
+      alt={`Foto de ${props.nombre}`} />
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>
+          "{props.testimonio}"</p>
+      </div>
+    </div>
+  )
+}
+
+export default Testimonio;
